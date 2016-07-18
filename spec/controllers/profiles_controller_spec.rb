cb16a94f2e3c430f7223d711f27c013e754cbb08
daljitcheema <daljit.cheema@me.com>
2016-07-18 22:15:18 +0100

    Fixed  controller for Profile

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ProfileController, type: :controller do
+RSpec.describe ProfilesController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
