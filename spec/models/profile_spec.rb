1ebdc66992e4b64b34c4f00fc7e3fa6332ebd5ba
tamlyn10 <tamlyn@ethicalswitch.com>
2016-07-20 09:51:54 +0100

    Added postcode, latitude and longitude to Profile model
    Testing that Geocoder is automatically updating latitude and longitude
    fields.

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:profile1){Profile.create!(postcode: 'EN11 8BX', latitude: "", longitude: "" )}
+  let(:profile2){Profile.create!(postcode: 'SG12', latitude: "", longitude: "" )}
+	
+  describe "searching for locational matches" do 
+    it "should return matches within 20 miles" do 
+      expect(profile1.latitude).to be_within(0.000001).of(51.7557328)
+    end
+  end
+
 end
