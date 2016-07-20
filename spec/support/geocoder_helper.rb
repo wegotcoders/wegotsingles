b165c5543b18644c8850b6f7d5ec3f9c0f364d6d
tamlyn10 <tamlyn@ethicalswitch.com>
2016-07-20 11:04:39 +0100

    rebased with dev after fixing migration files

--- spec/support/geocoder_helper.rb
+++ spec/support/geocoder_helper.rb
@@ -13,3 +13,31 @@ Geocoder::Lookup::Test.add_stub(
     }
   ]
 )
+
+Geocoder::Lookup::Test.add_stub(
+  "SG12 0SY", [
+    {
+      'latitude'     => 51.8378941,
+      'longitude'    => -0.028795,
+      'address'      => 'Thundridge, UK',
+      'state'        => 'Thundridge',
+      'state_code'   => 'England',
+      'country'      => 'United Kingdom',
+      'country_code' => 'GB'
+    }
+  ]
+)
+
+Geocoder::Lookup::Test.add_stub(
+  "CB2 1TN", [
+    {
+      'latitude'     => 52.20530369999999,
+      'longitude'    => 0.1166132,
+      'address'      => ' Cambridge, UK',
+      'state'        => ' Cambridge',
+      'state_code'   => 'England',
+      'country'      => 'United Kingdom',
+      'country_code' => 'GB'
+    }
+  ]
+)
