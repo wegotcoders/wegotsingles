dc12c590dfe336f5ed03a370800fc73a5235c1b1
tamlyn10 <tamlyn@ethicalswitch.com>
2016-07-22 15:08:16 +0100

    fixed seeds file and did a migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720164850) do
+ActiveRecord::Schema.define(version: 20160722095940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,17 @@ ActiveRecord::Schema.define(version: 20160720164850) do
   add_index "customers", ["email"], name: "index_customers_on_email", unique: true, using: :btree
   add_index "customers", ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true, using: :btree
   add_index "customers", ["username"], name: "index_customers_on_username", unique: true, using: :btree
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "sender_id"
+    t.integer  "receiver_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "replied_to_id"
+    t.boolean  "read"
+    t.boolean  "visible"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.datetime "created_at",  null: false
