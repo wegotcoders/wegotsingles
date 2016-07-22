6275db3b6482ddce1e8b8059eb38dbaa6d3d3610
Dan Garland <dan@dangarland.co.uk>
2016-07-22 17:32:01 +0100

    Fixing cukes

--- features/add_weight_imperial.feature
+++ features/add_weight_imperial.feature
@@ -2,7 +2,7 @@ Feature: Customer adds weight in imperial to Profile
 
 Background:
 	Given they have a profile
-	And they sign in to the system
+	And they have signed in
 	And they are on the Profile page
 
 Scenario: Customer adds their weight in imperial units to their Profile
--- features/step_definitions/edit_smoker_steps.rb
+++ features/step_definitions/edit_smoker_steps.rb
@@ -1,11 +1,5 @@
 Given(/^they have created a profile$/) do
-  @customer = Customer.create!(username: "fred", date_of_birth: Date.new(1990,12,12), email: "fred@bloggs.com",
-    password: "password", password_confirmation: "password",
-    confirmation_sent_at: Time.now - 1.day,
-    confirmed_at: Time.now)
-
-  @profile = Profile.create!(customer: @customer, smoker: true, name: Faker::Name.name)
-
+  step 'they have a profile'
 end
 
 When(/^they edit their smoking status$/) do
