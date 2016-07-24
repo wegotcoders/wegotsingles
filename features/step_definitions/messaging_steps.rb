07a5c7dffa7e3bb9a98cd2a1e283404f480ea4d8
kohrVid <kohrVid@zoho.com>
2016-07-24 19:21:45 +0100

    Customer can reply to messages

--- features/step_definitions/messaging_steps.rb
+++ features/step_definitions/messaging_steps.rb
@@ -22,6 +22,30 @@ When(/^he presses send$/) do
   click_on "Send"
 end
 
+When(/^he receives a message$/) do
+  @message = Message.create!(content: Faker::Lorem.paragraph, 
+	    		     sender_id: @profile_4.customer.id,
+			     receiver_id: @profile_1.customer.id)
+  visit current_path
+end
+
+When(/^he clicks the reply button$/) do
+  click_on "Reply"
+end
+
+Then(/^the message should appear beneath the original message$/) do
+  expect(page).to have_content(@content)
+  @reply = Message.find_by(content: @content)
+  expect(@reply.replied_to_id).to eq(@message.id)
+end
+
+Then(/^the message should appear in the original sender's inbox$/) do
+  click_on "Log Out"
+  step "Jess has signed in"
+  click_on "Inbox"
+  expect(page).to have_content(@content)
+end
+
 Then(/^he is redirected to his inbox$/) do
   expect(page).to have_content("Your Messages")
 end
@@ -37,4 +61,3 @@ Then(/^the message appears in the receiver's inbox$/) do
   click_on "Inbox"
   expect(page).to have_content(@content)
 end
-
